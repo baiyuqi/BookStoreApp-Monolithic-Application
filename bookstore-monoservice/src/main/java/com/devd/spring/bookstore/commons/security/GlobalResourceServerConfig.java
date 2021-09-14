567198a36abe8d6315ae002e3c5e23e859613838
baiyuqi <1353813001@qq.com>
2021-09-15 07:27:53 +0800

    ff

--- bookstore-monoservice/src/main/java/com/devd/spring/bookstore/commons/security/GlobalResourceServerConfig.java
+++ bookstore-monoservice/src/main/java/com/devd/spring/bookstore/commons/security/GlobalResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.devd.spring.bookstore.commons.security;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -13,8 +14,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -36,7 +39,14 @@ public class GlobalResourceServerConfig extends ResourceServerConfigurerAdapter 
 
     @Value("${security.jwt.public-key}")
     private Resource publicKey;
-
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
+    
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId("web").tokenServices(tokenServices);
+    }
+    
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
